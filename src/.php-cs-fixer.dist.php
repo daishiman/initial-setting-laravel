4e9319449fbed3549b01935391df9b366f37ba1c
daishiman <71166086+daishiman@users.noreply.github.com>
2022-12-15 10:28:16 +0900

    .php-cs-fixer.dist.php no

--- src/.php-cs-fixer.dist.php
+++ src/.php-cs-fixer.dist.php
@@ -1,14 +1,16 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 $finder = PhpCsFixer\Finder::create()
-    ->in([
-        __DIR__ . '/app',
-        __DIR__ . '/config',
-        __DIR__ . '/database/factories',
-        __DIR__ . '/database/seeders',
-        __DIR__ . '/routes',
-        __DIR__ . '/tests',
-    ]);
+                           ->in([
+                               __DIR__ . '/app',
+                               __DIR__ . '/config',
+                               __DIR__ . '/database/factories',
+                               __DIR__ . '/database/seeders',
+                               __DIR__ . '/routes',
+                               __DIR__ . '/tests',
+                           ]);
 
 $config = new PhpCsFixer\Config();
 
@@ -16,18 +18,27 @@ return $config
     ->setRiskyAllowed(true)
     ->setRules([
         '@PhpCsFixer:risky' => true,
-        'blank_line_after_opening_tag' => false,
-        'linebreak_after_opening_tag' => false,
+        'blank_line_after_opening_tag' => true,
+        'linebreak_after_opening_tag' => true,
         'declare_strict_types' => true,
-        'phpdoc_types_order' => [
-            'null_adjustment' => 'always_last',
-            'sort_algorithm' => 'none',
-        ],
         'no_superfluous_phpdoc_tags' => false,
         'global_namespace_import' => [
             'import_classes' => true,
             'import_constants' => true,
             'import_functions' => true,
+        ],
+        'ordered_imports' => [
+            'sort_algorithm' => 'alpha',
+            'imports_order' => [
+                'class',
+                'function',
+                'const',
+            ],
+        ],
+        'no_unused_imports' => true,
+        'phpdoc_types_order' => [
+            'null_adjustment' => 'always_last',
+            'sort_algorithm' => 'none',
         ],
         'php_unit_test_case_static_method_calls' => [
             'call_type' => 'this'
@@ -44,5 +55,54 @@ return $config
         'mb_str_functions' => true,
         'simplified_if_return' => true,
         'simplified_null_return' => true,
+        'binary_operator_spaces' => [
+            'align_double_arrow' => true,
+            'align_equals' => true,
+        ],
+        'blank_line_before_return' => true,
+        'compact_nullable_typehint' => true,
+        'concat_space' => [
+            'spacing' => 'single'
+        ],
+        'function_typehint_space' => true,
+        'lowercase_cast' => true,
+        'lowercase_constants' => true,
+        'lowercase_keywords' => true,
+        'method_argument_space' => [
+            'ensure_fully_multiline' => false,
+            'keep_multiple_spaces_after_comma' => false,
+        ],
+        'method_separation' => true,
+        'new_with_braces' => true,
+        'no_empty_comment' => true,
+        'no_empty_phpdoc' => true,
+        'no_empty_statement' => true,
+        'no_extra_consecutive_blank_lines' => [
+            'tokens' => ['extra']
+        ],
+        'no_leading_import_slash' => true,
+        'no_leading_namespace_whitespace' => true,
+        'no_mixed_echo_print' => [
+            'use' => 'echo'
+        ],
+        'no_multiline_whitespace_around_double_arrow' => true,
+        'no_spaces_after_function_name' => true,
+        'no_spaces_inside_parenthesis' => true,
+        'no_superfluous_elseif' => true,
+        'no_useless_else' => true,
+        'no_trailing_comma_in_list_call' => true,
+        'no_trailing_comma_in_singleline_array' => true,
+        'no_trailing_whitespace' => true,
+        'no_unneeded_control_parentheses' => [
+            'statements' => ['break', 'clone', 'continue', 'echo_print', 'return', 'switch_case', 'yield']
+        ],
+        'no_unneeded_curly_braces' => true,
+        'no_unneeded_final_method' => true,
+        'no_whitespace_before_comma_in_array' => true,
+        'no_whitespace_in_blank_line' => true,
+        'trim_array_spaces' =>  true,
+        'unary_operator_spaces' => true,
+        'visibility_required' => true,
+        'whitespace_after_comma_in_array' => true,
     ])
     ->setFinder($finder);
